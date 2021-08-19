ebc0214ae48894320234b8be3efd7f519f3cc9cc
Yauheny Bychkou <375445433672@yandex.by>
2021-08-19 15:37:09 +0300

    ready101percent

--- script.js
+++ script.js
@@ -37,3 +37,28 @@ const myLesson = [
 
 go(myLesson);
 console.log(myLesson);
+
+/* Напишите функцию на JS. Цель: Убрать все объекты с типом additional, а для basic очки уменьшить в двое.
+
+Cоздать новый массив, оставляя исходные данные неизменными */
+
+const myLessonn = [
+  { lesson: 1, type: "basic", points: 2 },
+  { lesson: 2, type: "additional", points: 4 },
+  { lesson: 3, type: "basic", points: 6 },
+  { lesson: 4, type: "additional", points: 3 },
+  { lesson: 5, type: "basic", points: 4 },
+  { lesson: 6, type: "basic", points: 2 },
+  { lesson: 7, type: "additional", points: 2 },
+  { lesson: 8, type: "basic", points: 6 },
+  { lesson: 9, type: "basic", points: 4 },
+  { lesson: 10, type: "basic", points: 6 },
+  { lesson: 11, type: "additional", points: 5 },
+  { lesson: 12, type: "basic", points: 2 },
+  { lesson: 13, type: "additional", points: 2 },
+  { lesson: 14, type: "basic", points: 4 },
+  { lesson: 15, type: "additional", points: 1 },
+  { lesson: 16, type: "additional", points: 7 },
+];
+myLessonn.filter((item) => item.type !== "additional").map((item) => ({ ...item, points: item.points / 2 }));
+console.log(myLessonn);
