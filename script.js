a32e89042a0cc2de2e59d5395c62aa64fa73310f
Yauheny Bychkou <375445433672@yandex.by>
2021-08-24 08:04:07 +0300

    techniacalmistake

--- script.js
+++ script.js
@@ -1,52 +1,44 @@
 "use strict";
-let option;
-let option2;
-let option3;
-let text = document.querySelector("#text");
-let text1 = document.querySelector("#text-1");
-let city = document.querySelector("#city");
-let country = document.querySelector("#country");
-option = document.createElement("option");
-option2 = document.createElement("option");
-option3 = document.createElement("option");
-city.add(option);
-city.add(option2);
-city.add(option3);
+let a = document.querySelector("#a"),
+  b = document.querySelector("#b"),
+  btnSum = document.querySelector("#sum"),
+  btnMult = document.querySelector("#mult"),
+  res = document.querySelector("#res"),
+  option = document.querySelector("option");
+option.text = "Новый пункт";
+option.value = "new_iten";
 
-country.addEventListener("change", function (e) {
-  if (e.target.value === "rus") {
-    option.textContent = "Москва";
-    option2.textContent = "Питер";
-    option3.textContent = "Казань";
-    text1.textContent = "Россия";
-    text1.style.display = "none";
-    text.style.display = "none";
-  } else if (e.target.value === "bel") {
-    option.textContent = "Минск";
-    option2.textContent = "Могилёв";
-    option3.textContent = "Гродно";
-    text1.textContent = "Беларусь";
-    text1.style.display = "none";
-    text.style.display = "none";
-  } else if (e.target.value === "uk") {
-    option.textContent = "Киев";
-    option2.textContent = "Львов";
-    option3.textContent = "Одесса";
-    text1.textContent = "Украина";
-    text1.style.display = "none";
-    text.style.display = "none";
-  } else if (e.target.value === "jap") {
-    option.textContent = "Токио";
-    option2.textContent = "Киото";
-    option3.textContent = "Иокогама";
-    text1.textContent = "Япония";
-    text1.style.display = "none";
-    text.style.display = "none";
-  }
+document.querySelector("#select").add(option);
 
-  document.querySelector("#city").addEventListener("change", function (e) {
-    text.textContent = e.target.value;
-    text.style.display = "initial";
-    text1.style.display = "initial";
-  });
-});
+const calculator = {
+  sum: function () {
+    let summ = 0;
+    let pow;
+    this.a = +a.value;
+    this.b = +b.value;
+    pow = Math.pow(this.a, this.b);
+
+    while (pow > 0) {
+      summ += pow % 10;
+      pow = Math.floor(pow / 10);
+    }
+    return summ;
+  },
+
+  mult: function () {
+    this.a = +a.value;
+    this.b = +b.value;
+    return Math.pow(this.a, this.b);
+  },
+
+  readValues: function () {
+    btnSum.addEventListener("click", function () {
+      res.value = calculator.sum();
+    });
+    btnMult.addEventListener("click", function () {
+      res.value = calculator.mult();
+    });
+  },
+};
+
+calculator.readValues();
